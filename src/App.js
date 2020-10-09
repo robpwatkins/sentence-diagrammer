296122665e8b549e8a2caa308aba834b5498d172
robpwatkins <robpwatkins@gmail.com>
2020-10-08 21:10:32 -0500

    anddd now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Diagrammer from './components/Diagrammer';
 
 function App() {
   return (
     <div className="App">
+      <Diagrammer />
     </div>
   );
 }
--- src/components/Diagrammer.js
+++ src/components/Diagrammer.js
@@ -1,7 +1,11 @@
-import react from 'react';
+import React from 'react';
 
 const Diagrammer = () => {
-
+    return (
+        <div className="diagrammer">
+            <h3>Diagrammer, here.</h3>
+        </div>
+    )
 }
 
 export default Diagrammer;
